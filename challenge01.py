25ff11179823181d6f03cfbaa1b521393c461854
Paul Scherr <monalisa@alta3.com>
2021-04-01 16:18:55 +0000

    python and excel

---
+++ challenge01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" build your own multiple choice questionaire """
+
+def main():
+        # Delcare variables
+        # YourName = input()
+
+
+        # print some stuff
+        print("Hello, what is your name?")
+        YourName = input()
+        YourName = str(YourName)
+        print("Hi, " + str(YourName))
+
+        # receive some answers
+if __name__ == "__main__":
+    main()
---
+++ smallscript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+AUTHOR: Paul
+"""
+
+def main():
+    """OUR MAIN PROGRAM"""
+    mynote = "print this string"
+    print(mynote)
+
+main()
